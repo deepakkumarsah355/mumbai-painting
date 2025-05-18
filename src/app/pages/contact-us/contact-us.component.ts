6f58c42fcdac76ee4e889efd82812f66b767cc22
deepakkumarsah355@gmail.com <deepakkumarsah355@gmail.com>
2025-05-18 17:18:06 +0530

    image added

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+onSubmit(_t15: NgForm) {
+throw new Error('Method not implemented.');
+}
 
 }
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ServicesComponent {
 
+   services = [
+    {
+      title: 'Interior Design & Painting',
+      description: 'Transform your interiors with stunning colors, textures, and elegant design that reflect your taste and personality.',
+      image: 'https://images.unsplash.com/photo-1615874959474-d609969a20ed?auto=format&fit=crop&w=800&q=80'
+    },
+    {
+      title: 'Exterior Services',
+      description: 'Boost your home’s curb appeal and protection with our high-quality exterior painting and care services.',
+      image: 'https://images.unsplash.com/photo-1615874959474-d609969a20ed?auto=format&fit=crop&w=800&q=80'
+    },
+    {
+      title: 'Furniture Finishing',
+      description: 'Give your furniture a modern finish or restore its vintage charm with professional-grade techniques and care services.',
+      image: 'https://images.unsplash.com/photo-1615874959474-d609969a20ed?auto=format&fit=crop&w=800&q=80'
+    }
+  ];
 }
